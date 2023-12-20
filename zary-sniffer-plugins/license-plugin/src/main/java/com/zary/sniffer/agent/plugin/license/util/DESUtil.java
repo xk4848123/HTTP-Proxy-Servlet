757a5225179f4696f691bb88008a7bdeaf23e680
chendi <15395068264@163.com>
2023-12-20 14:55:33 +0800

    fix:修复编码问题

--- zary-sniffer-plugins/license-plugin/src/main/java/com/zary/sniffer/agent/plugin/license/util/DESUtil.java
+++ zary-sniffer-plugins/license-plugin/src/main/java/com/zary/sniffer/agent/plugin/license/util/DESUtil.java
@@ -3,24 +3,25 @@ package com.zary.sniffer.agent.plugin.license.util;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class DESUtil {
     private static final String ALGORITHM = "DES";
 
     public static String encrypt(String plainText, String secretKey) throws Exception {
-        SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
+        SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encrypted = cipher.doFinal(plainText.getBytes());
+        byte[] encrypted = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
         return Base64.getEncoder().encodeToString(encrypted);
     }
 
     public static String decrypt(String encryptedText, String secretKey) throws Exception {
-        SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
+        SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] original = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        byte[] original = cipher.doFinal(Base64.getDecoder().decode(encryptedText.getBytes(StandardCharsets.UTF_8)));
         return new String(original);
     }
 
